6f67e1c367541f8f6cecdc3063c64c353dce937e
Jahid458 <Jahidul898@outlook.com>
2026-04-20 21:53:58 +0600

    using genric Array

---
+++ src/generic.ts
@@ -0,0 +1,36 @@
+// dynamicaaly generealize: Generic
+
+type GenericArray<T> = Array<T> //typescript parameter tai codebase T hisebe dey
+
+// const freinds: String[] = ['Mr. X','Mr. Y','Mr. Z']
+const freinds: GenericArray<string> = ['Mr. X','Mr. Y','Mr. Z']
+
+// const rollNumber: number[] = [3, 5 ,11]
+const rollNumber: GenericArray<number> = [3, 5 ,11]
+
+// const isEligibleList: boolean[] = [true, false, true]
+const isEligibleList: GenericArray<boolean> = [true, false, true]
+
+const sqrFunc = (value:number) => {
+    return value * value
+}
+//Array Generics
+type Coordinates<X,Y> = [X,Y]
+
+const coordinates1:Coordinates<number,number> = [20,30]
+const coordinates2:Coordinates<string,string> = ['20','30']
+
+
+//Object Generics
+
+const userList:GenericArray<{name: string,age:number}> = [
+    {
+        name: 'Mr. X',
+        age: 54
+    },
+    {
+        name: 'Mr. Y',
+        age: 30
+    }
+]
+
